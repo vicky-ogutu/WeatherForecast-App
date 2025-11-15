313985fd3427d9aae61d47cf561113289d83f907
vicky-ogutu <vikogutu77@gmail.com>
2025-11-15 12:41:07 +0300

    Added Api

---
+++ app/src/main/java/com/example/weatherapp/Api/WeatherApiService.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapp.Api
+import com.example.weatherapp.models.WeatherResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+
+    @GET("weather")
+    suspend fun getWeather(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): WeatherResponse
+}
